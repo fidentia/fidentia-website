2def900ff68f9c13fed486e8b3b6fc29e4abc91c
Giovanni Sena <giovanni.sena@ikatec.com.br>
2023-01-08 23:43:45 -0300

    First commit / Menu responsivo

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import styles from './header.module.scss';
+
+export default function Header(){
+    const [activeMenu, setActiveMenu] = useState(false);
+    const handleToggle = () =>{
+        setActiveMenu(!activeMenu);
+    }
+
+
+    return (
+        <header className={styles.header}>
+            <Link href="/">
+                <span><img className={styles.logo} src="/images/fidentia_logo.png" alt="logomarca fidentia" /></span> 
+            </Link>
+          <nav className={styles.nav}>
+            <button className={styles.button_mobile} onClick={()=>handleToggle()}>
+                <img src="/images/hamburguer.svg" alt="hamburguer logo" />
+            </button>
+            <ul className={activeMenu ? styles.active : styles.menu}>
+                <Link href="/">
+                    <li>Sobre</li>
+                </Link>
+                <Link href="/">
+                    <li>Produtos</li>
+                </Link>
+                <Link href="/">
+                    <li>Portfólio</li>
+                </Link>
+                <Link href="/">
+                    <li>Contato</li>
+                </Link>
+            </ul>
+          </nav>
+        </header>
+    )
+}
