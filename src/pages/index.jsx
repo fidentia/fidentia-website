0b14a1330ba13f40a59f2bac4fdb627e3db9d5f1
Giovanni Sena <giovanni.sena@ikatec.com.br>
2023-01-15 12:43:28 -0300

    [FEATURE] add paginacao e perPage dos posts

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,24 +7,58 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { useState } from "react";
 
 export default function Home({postsPagination}) {
+  
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
   const [posts, setPosts] = useState(postsPagination?.results);
 
+  const handlePagination = () => {
+    fetch(nextPage)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        const formattedData = data.results.map(post => {
+          return {
+            uid: post.uid,
+            first_publication_date: format(new Date(post.first_publication_date), "dd MMM, yyyy", {
+              locale: ptBR
+            }),
+            data: {
+              title: post.data.title
+            },
+          };
+        });
+        setPosts([...posts, ...formattedData]);
+        setNextPage(data.next_page);
+      })
+  }
+  // const handlePagination = () => {
+  //   fetch(nextPage)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       data.results.map(post =>{
+  //         console.log(post.data.title)
+  //       })
+  //     })
+  // }
   return (
     <>
       <Header />
-      <section className={styles.container}>
-        {posts.map(post => (
-          <div className={styles.post}>
-            <div>
-                <h3>{post.data.title}</h3>
-                <p>{post.first_publication_date}</p>
+      <main className={styles.container}>
+        <section className={styles.container_posts}>
+          {posts.map(post => (
+            <div key={post.uid} className={styles.post}>
+              <div>
+                  <h3>{post.data.title}</h3>
+                  <p>{post.first_publication_date}</p>
+              </div>
+              <div>
+                  <button>Ver artigo</button>
+              </div>
             </div>
-            <div>
-                <button>Ver artigo</button>
-            </div>
-          </div>
-        ))}
+          ))}
+          <button onClick={()=>handlePagination()}>Ver mais</button>
         </section>
+      </main>
       {/* <Posts /> */}
     </>
   )
@@ -36,11 +70,13 @@ export async function getStaticProps() {
   const postsResponse = await client.get([
     prismic.predicate.at('document.type', 'posts')
   ], {
-    fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
-    pageSize: 1,
-    orderings: '[document.first_publication_date desc]'
-  })
-  
+    orderings: {
+      field: 'document.first_publication_date',
+      direction: 'asc'
+    },
+    fetchLinks: ['posts.title', 'posts.subtitle', 'posts.author'],
+  });
+
   const posts = postsResponse.results.map(post => {
     return{
       uid: post.uid,
--- src/services/prismic.jsx
+++ src/services/prismic.jsx
@@ -1,8 +1,12 @@
 import * as prismic from '@prismicio/client'
 
-export function getPrismicClient(req){
+export function getPrismicClient(){
+
   const client = prismic.createClient("https://space-traveling-gio.cdn.prismic.io/api/v2", {
-    accessToken: process.env.PRISMIC_ACCESS_TOKEN
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    defaultParams:{
+      pageSize: 2
+    }
   });
 
   return client;
