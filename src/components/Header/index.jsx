57d6ba8c588baa01dcaf3c66f3bda220abd3925e
Giovanni Sena <giovanni.sena@ikatec.com.br>
2023-01-20 17:27:59 -0300

    [FEATURE] ajustando header e inserindo logos para social media

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -17,7 +17,7 @@ export default function Header(){
             </Link>
           <nav className={styles.nav}>
             <button className={styles.button_mobile} onClick={()=>handleToggle()}>
-                <img src="/images/hamburguer.svg" alt="hamburguer logo" />
+                <img src="/images/hamburguerLogo.svg" alt="hamburguer logo" />
             </button>
             <ul className={activeMenu ? styles.active : styles.menu}>
                 {activeMenu ? (
@@ -37,6 +37,22 @@ export default function Header(){
                 <Link href="/">
                     <li>A fidentia</li>
                 </Link>
+                {activeMenu ? (
+                    <section className={styles.social_medias_logos}>
+                        <figure>
+                            <img src="/images/instagramLogo.svg" alt="instagram logo" />
+                        </figure>
+                        <figure>
+                            <img src="/images/facebookLogo.svg" alt="facebook logo" />
+                        </figure>
+                        <figure>
+                            <img src="/images/linkedinLogo.svg" alt="linkedin logo" />
+                        </figure>
+                        <figure>
+                            <img src="/images/youtubeLogo.svg" alt="youtube logo" />
+                        </figure>
+                    </section>
+                ) : null}
             </ul>
           </nav>
         </header>
