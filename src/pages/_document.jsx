1d8b0138e71ffd8bb237722aae2bf1daf3f743c0
Giovanni Sena <giovanni.sena@ikatec.com.br>
2023-02-04 17:57:47 -0300

    [FIX] removendo meta tags do _document

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,12 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" />        
-        <meta name="description" content="Soluções completas em seguros para pesquisas clínicas. Oferecemos uma suíte completa de soluções e serviços com tudo o que você precisa sobre seguro para o mundo das pesquisas clínicas." />
-        <meta property="og:title" content="Home" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.fidentia.com.br/" />
-        <meta property="og:description" content="Soluções completas em seguros para pesquisas clínicas" />
-        <meta property="og:site_name" content="Fidentia" />
+        
       </Head>
       <body>
         <Main />
