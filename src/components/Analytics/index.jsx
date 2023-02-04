84744914a8850d8b611d357123caf0b6322bebf7
Giovanni Sena <giovanni.sena@ikatec.com.br>
2023-02-04 20:45:03 -0300

    [FIX] ajustando process variavel

--- src/components/Analytics/index.jsx
+++ src/components/Analytics/index.jsx
@@ -5,7 +5,7 @@ export default function Analytics(){
         <>
             <Script
             strategy="afterInteractive"
-            src={`https://www.googletagmanager.com/gtag/js?id=${proccess.env.NEXT_PUBLIC_GA_ID}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
             />
             <Script
             strategy="afterInteractive"
@@ -14,7 +14,7 @@ export default function Analytics(){
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${proccess.env.NEXT_PUBLIC_GA_ID}', {
+                    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
                         page_path: window.location.pathname,
                     });
                 `
