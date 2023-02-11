d2cde200c3316e80e45e517497d5ddcdd99c9c7c
wesleydesouza <wesley_duarte122@hotmail.com>
2023-02-11 18:43:14 -0300

    feat: adiciona favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700&display=swap" rel="stylesheet" /> 
+        <link rel="icon" type="image/jpeg" sizes="32x32" href="/images/favicon.jpeg"/>
       </Head>
       <body>
         <Main />
