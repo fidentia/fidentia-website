eddd7910676efa5b8c5e4fc55a369b6706256427
gsilva <gsilva@essapiens.com.br>
2023-03-12 17:48:33 -0300

    feat: inserindo importacao bootstrap no app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,8 @@
 import '../styles/global.scss';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
